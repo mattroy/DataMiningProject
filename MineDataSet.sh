fed58dc7343cb97dfe29749745f09b4049dc32fe
mattroy <mattroy@ccs.neu.edu>
2014-12-08 03:04:54 -0500

    update mine script.:

--- MineDataSet.sh
+++ MineDataSet.sh
@@ -7,6 +7,11 @@ python ./scripts/eval_features.py svm_config
 echo "-----------------FINISHED EVAL FEATURE--------------------------"
 echo
 
+echo "make lib-svm"
+./libsvm-3.20/make
+echo "----------------MADE LIB SVM------------------------------------"
+
+
 #2. run svm-predict
 echo "run the svm-predict"
 ./libsvm/svm-predict 
@@ -17,5 +22,5 @@ echo
 #3. Parse the svm results and generate a prediction file
 echo "run the parsing"
 python ./scripts/parse_prediction.py svm_config
-echo "-----------------FINISHED ParseMap--------------------------"
+echo "-----------------FINISHED ParseMap-----------------------------"
 echo
