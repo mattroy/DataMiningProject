f578068fa45d5508650155db2b79d54dcfb25ffe
mattroy <mattroy@ccs.neu.edu>
2014-12-08 02:17:00 -0500

    remove training from the mine script.

--- MineDataSet.sh
+++ MineDataSet.sh
@@ -1,38 +1,21 @@
-
 #!/bin/bash
 set -e
 
-
-#1. run the gen_features script
-echo "run the features generator script"
-python ./scripts/feature_gen.py svm_config
-echo "-----------------FINISHED FEATURE GENERATOR--------------------------"
-echo
-
-
-#2. run eval_features
+#1. run eval_features
 echo "run the eval features script"
 python ./scripts/eval_features.py
 echo "-----------------FINISHED EVAL FEATURE--------------------------"
 echo
 
-
-#3. run svm-train
-echo "run the svm-train"
-./libsvm/svm-train
-echo "-----------------FINISHED SVM train--------------------------"
-echo
-
-
-#4. run svm-predict
+#2. run svm-predict
 echo "run the svm-predict"
 ./libsvm/svm-predict
 echo "-----------------FINISHED SVM predict--------------------------"
 echo
 
 
-#5. run Saoni's new script
-echo "run the parseMap"
+#3. Parse the svm results and generate a prediction file
+echo "run the parsing"
 python ./scripts/parse_prediction.py svm_config
 echo "-----------------FINISHED ParseMap--------------------------"
 echo
